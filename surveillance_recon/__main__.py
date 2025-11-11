8147c369c93a94757532724a665b6fc2efebbbfd
joyelkhan <joyelkhan2@gmail.com>
2025-11-11 23:29:12 +0500

    Fix package structure: Move all modules into surveillance_recon/ directory and add __main__.py

---
+++ surveillance_recon/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+surveillance_recon/__main__.py
+Entry point for running the package as a module: python -m surveillance_recon
+"""
+
+from surveillance_recon.main import main
+
+if __name__ == "__main__":
+    main()
